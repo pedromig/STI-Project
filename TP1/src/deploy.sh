59bf0ff2c061dba46522f16ee0d61828b3da8db4
Pedro <pedromiguelrodrigues2000@gmail.com>
2022-03-08 21:55:58 +0000

    Adding initial configuration of the Apache Server

--- TP1/src/deploy.sh
+++ TP1/src/deploy.sh
@@ -11,8 +11,18 @@
 # Paths to the configuration files and global variables
 
 ## Coimbra
-COIMBRA_CA_CERT_ENTITIES="apache "
+COIMBRA_CA_CERT_ENTITIES="apache"
+COIMBRA_CA_CERT_ENTITIES_PKCS12="apache"
 COIMBRA_SSL_CONF="coimbra/openssl.cnf"
+
+## Lisbon
+LISBON_APACHE_SSL_CONF="lisbon/apache-ssl.conf"
+LISBON_APACHE_CONF="lisbon/apache.conf"
+LISBON_APACHE_INDEX_HTML="lisbon/index.html"
+LISBOA_HOSTS_FILE="lisbon/hosts"
+LISBOA_APACHE_CERT="apache.crt"
+LISBOA_APACHE_CERT_KEY="apache.key"
+LISBOA_APACHE_CA_FILE="cacert.pem"
 
 # Required Programs (packages downloadable with apt-get, 
 # may be different for # other package managers)
@@ -43,23 +53,26 @@ help() {
 
 # Coimbra CA/OCSP/VPN Server Configurations
 coimbra() {   
-  # Files
+  # OpenSSL Config Directory
+  ssldir="/etc/ssl"               # Path to the ssl config file directory
+
+  # CA Files
   keybits=2048                    # Number of bits for the private key 
   days=3650                       # Certificate validity
   capk="cakey.pem"                # CA RSA private key file name
   cacert="cacert.pem"             # CA certificate
+  cacsr="ca.crt"                  # CA CSR (Certificates Signing Request)
 
-  # Directories
-  ssldir="/etc/ssl"               # Path to the ssl config file directory
+  # CA Directories
   cadir="/etc/pki/CA";            # Path to the CA main directory
   newcerts="/etc/pki/CA/newcerts" # Newly generated ceritificates go here
   certsdir="/etc/pki/CA/certs"    # Certitificates directory
   capriv="/etc/pki/CA/private"    # Path to the CA private key directory
+  exportdir="/etc/pki/CA/export"  # Directory where pk12 certificates
 
   # Openssl and CA configuration deployment
   sudo cp -f "${COIMBRA_SSL_CONF}" "${ssldir}"
-  sudo mkdir -p "${capriv}"
-  sudo mkdir -p "${certsdir}" "${newcerts}"
+  sudo mkdir -p "${capriv}" "${certsdir}" "${newcerts}" "${exportdir}"
 
   # Private CA creation
 
@@ -73,11 +86,13 @@ coimbra() {
   cd "${cadir}"
   sudo openssl req \
       -new -key "${privdir}/${capk}" \
-      -out ca.crt
+      -out "${cacsr}" 
 
   ## Create self-signed CA certificate
-  sudo openssl x509 -req -days "${days}" \
-      -in ca.crt \
+  sudo openssl x509 \
+      -req \
+      -days "${days}" \
+      -in "${cacsr}" \
       -out "${cacert}" \
       -signkey "${privdir}/${capk}"
 
@@ -88,6 +103,7 @@ coimbra() {
  
   # Generate X.509 certificates for all the entities 
   for entity in ${COIMBRA_CA_CERT_ENTITIES}; do 
+
     ## Create entity private key
     cd "${capriv}"
     sudo openssl genrsa \
@@ -97,7 +113,8 @@ coimbra() {
     ## Create Entity CSR (Certificate Signing Request) 
     cd "${cadir}"
     sudo openssl req \
-        -new -key "${privdir}/${entity}.key" \
+        -new \
+        -key "${privdir}/${entity}.key" \
         -out "${entity}.csr"
 
     ## Create X.509 certificate for this entity
@@ -107,11 +124,56 @@ coimbra() {
         -keyfile "${capriv}/${capk}" \
         -out "${certsdir}/${entity}.crt"
   done 
+
+  # Generate X.509 PKCS##12 certificates for all entities who need it 
+  for entity in ${COIMBRA_CA_CERT_ENTITIES_PKCS12}; do 
+
+    ## Export Key in the PKCS##12 format
+    sudo openssl pkcs12 \
+        -export \
+        -out "${exportdir}/${entity}.p12" \
+        -inkey "${capriv}/${entity}.key" \
+        -in "${certs}/${entity}.crt" \
+        -certfile "${cacert}"      
+  done 
 }
 
 # Lisbon Server Configurations
-lisbon() {
-  echo "Lisbon"
+lisbon() { 
+  # Apache Config Directories
+  sites="/etc/apache2/sites-available"
+  public_html="/var/www/apache/public_html"
+
+  # Certificate Related Directories 
+  certdir="/etc/pki/CA/certs"
+  keydir="/etc/pki/CA/private"
+  cadir="/etc/pki/CA/"
+
+  # Enable Apache2 modules
+  sudo e2enmod ssl
+  
+  # System Directories 
+  etc="/etc"
+
+  # Setup folders/files needed for the configuration deployment
+  sudo mkdir -p "${certdir}" "${keydir}" "${cadir}"
+  sudo cp -f "${LISBON_APACHE_SSL_CONF}" "${sites}"
+  sudo cp -f "${LISBON_APACHE_CONF}" "${sites}"
+
+  sudo mkdir -p "${public_html}"
+  sudo cp -f ${LISBON_APACHE_INDEX_HTML} "${public_html}"
+  sudo chown -R $USER:$USER "${public_html}" 
+  sudo chmod -R 755 "${public_html}" 
+
+  sudo cp -f "${LISBON_APACHE_CERT}" "${certdir}"
+  sudo cp -f "${LISBON_APACHE_CERT_KEY}" "${keydir}"
+  sudo cp -f "${LISBON_APACHE_CA_FILE}" "${cadir}"
+  sudo cp -f "${LISBON_HOSTS_FILE}" "${etc}"
+
+  # Start/Restart apache2 service after setting up config files
+  sudo a2ensite "${LISBON_APACHE_SLL_CONF}"
+  sudo a2ensite "${LISBON_APACHE_CONF}"
+  sudo systemctl start apache2.service || sudo systemctl reload apache2.service
 }
 
 # Road Warriot Configurations
